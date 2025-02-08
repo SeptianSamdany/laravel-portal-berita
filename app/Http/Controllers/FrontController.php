afda61710593c25270aa5d06b9ca1cb0dbe3b6a0
SeptianSamdany <227007072@student.unsil.ac.id>
2025-02-08 17:42:57 +0700

    Add Front Controller

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ArticleNews;
+use App\Models\Author;
+use App\Models\BannerAdvertisement;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all(); 
+
+        $articles = ArticleNews::with(['category'])
+        ->where('is_featured', 'not_featured')
+        ->latest()
+        ->take(3)
+        ->get(); 
+
+        $featured_articles = ArticleNews::with(['category'])
+        ->where('is_featured', 'featured')
+        ->inRandomOrder()
+        ->take(3)
+        ->get(); 
+
+        $authors = Author::all(); 
+
+        $bannerads = BannerAdvertisement::where('is_active', 'active')
+        ->where('type'. 'banner')
+        ->inRandomOrder()
+        // ->take(1)
+        ->first(); 
+
+        return view('front.index', compact('categories', 'articles', 'authors', 'featured_articles', 'bannerads')); 
+    }
+}
